29125e3b88a6953606b002fb900816f98ef48b9f
da-bean-create <sean.slisar@education.nsw.gov.au>
2026-05-18 12:52:11 +1000

    Started
    created necessary files, added UI starter code

---
+++ main.py
@@ -0,0 +1,11 @@
+def main_menu()
+  while true:
+    print("------Data Menu------")
+    print("1. View Raw Dataset")
+    print("2. Dataset Representation")
+    print("3. Filter Data Via Search")
+    print("4. Update Data Entry")
+    
+    user_choice = input("Select an Option")
+    if user_choice == "2":
+        print("ok")
