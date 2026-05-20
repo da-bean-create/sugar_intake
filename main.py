e3dd399ceca4449217d6056f260995752c8bbe7b
da-bean-create <sean.slisar@education.nsw.gov.au>
2026-05-21 09:18:32 +1000

    did a lot of work
    added search function, second main menu

--- main.py
+++ main.py
@@ -5,13 +5,28 @@ from data_module import(
 def main_menu():
   while true:
     print("------Data Menu------")
-    print("1. View Raw Dataset")
-    print("2. Dataset Representation")
-    print("3. Filter Data Via Search")
-    print("4. Update Data Entry")
+    print(" 1. View Raw Dataset")
+    print(" 2. Dataset Representation")
+    print(" 3. Update Data Entry")
+    print(" 4. Exit")
     
-    user_choice = input("Select an Option")
+    user_choice = input("Select an Option: ")
     if user_choice == "1":
-        raw_data()
+      print(raw_data())
+      print("---------Raw Data Menu---------")
+      print(" 1. Filter Data")
+      print(" 2. Return to Main Menu")
+      user_choice2 = input("Choose an Option: ")
+      if user_choice2 == "1":
+         column_choice = input("Which Catagorie/s Would you Like to Filter By?")
+         
+      elif user_choice2 == "2":
+        print(main_menu())
+    elif user_choice == "2":
+      print()
+    elif user_choice == "3":
+      print()
+    elif user_choice == "4":
+        update_data()
 
 print(main_menu())
